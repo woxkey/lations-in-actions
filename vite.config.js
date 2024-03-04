d2c3ec67914229cec38c781f29d8261a8747550a
Alen Bolatov <bolatovalen@gmail.com>
2024-03-04 10:57:03 -0500

    add new hash on build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {hash} from './src/functions'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  assetsInclude: ['**/*.jpg']
+  assetsInclude: ['**/*.jpg'],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]` + hash + `.js`,
+        chunkFileNames: `[name]` + hash + `.js`,
+        assetFileNames: `[name]` + hash + `.[ext]`
+      }
+    }
+  }
 })
